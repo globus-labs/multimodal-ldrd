4bfe8f675998e987714e652b2729675915b28341
Logan Ward <ward.logan.t@gmail.com>
2019-11-01 17:43:03 -0500

    Added regularization
    Also refactored to use smaller arrays,
    at the cost of computational efficiency

--- multimodal/keras.py
+++ multimodal/keras.py
@@ -12,7 +12,8 @@ class SingleModalFactorization(Layer):
     """A non-negative matrix factorization that allows the constituent signals
     to shift across different measurements of the signal"""
 
-    def __init__(self, n_components=2, fit_shift=False, **kwargs):
+    def __init__(self, n_components=2, fit_shift=False,
+                 regularizer=None, **kwargs):
         """
         Args:
              n_components (int): Number of components to learn
@@ -21,9 +22,11 @@ class SingleModalFactorization(Layer):
         super().__init__(**kwargs)
         self.n_components = n_components
         self.fit_shift = fit_shift
+        self.regularizer = regularizer
 
         # Placeholders for the weights
         self.components = self.contributions = self.shift = None
+        self.shifted_contributions = None
 
         # Placeholders for input shape information
         self.n_samples = self.n_channels = None
@@ -39,73 +42,76 @@ class SingleModalFactorization(Layer):
             name='components',
             shape=(self.n_components, self.n_channels),
             constraint=NonNeg(),
-            initializer='uniform'
+            initializer='uniform',
+            regularizer=self.regularizer
         )
         self.contributions = self.add_weight(
             name='contributions',
             shape=(self.n_samples, self.n_components),
             constraint=NonNeg(),
-            initializer='zeros'
+            initializer='uniform',
+            regularizer=self.regularizer
         )
         if self.fit_shift:
             self.shift = self.add_weight(
                 name='shift',
                 shape=(self.n_samples, self.n_components),
-                initializer='zeros'
+                initializer='uniform',
+                regularizer=self.regularizer,
             )
 
     def call(self, inputs, **kwargs):
         if self.fit_shift:
-            # Compute the shift matrix
+            # Make a shift matrix
             #  The shift matrix is a matrix where
             #   shift[i, j] = j - i
             #  Allows you to easily compute the distance of a specific
             #  point from all points in a pattern. We can use that
             #  distance to create interpolators
-            # TODO (wardlt): Replace this with a spline interpolator?
             k_matrix = np.zeros((self.n_channels, self.n_channels))
             for k in range(-self.n_channels, self.n_channels, 1):
                 kiu = np.triu_indices_from(k_matrix, k)
                 k_matrix[kiu] = k
-            k_matrix = k_matrix[None, None, :, :]
-            k_matrix = np.tile(k_matrix, (self.n_samples, self.n_components, 1, 1))
-            k_matrix = K.variable(k_matrix)
+            k_matrix = K.constant(k_matrix)
 
-            # Compute the distance of each shift point from
-            #  every point in each NMF component
-            tiled_shift_matrix = K.expand_dims(K.expand_dims(self.shift, -1), -1)
-            tiled_shift_matrix = K.tile(tiled_shift_matrix,
-                                        (1, 1, self.n_channels, self.n_channels))
-            distance_matrix = K.abs(tiled_shift_matrix - k_matrix)
+            # Compute the shifts from each component
+            shifted_components = []
+            for c in range(self.n_components):
+                # Get the component and the contribution
+                my_component = self.components[c, :]  # shape: (n_channels)
+                my_contribution = self.contributions[:, c]  # shape: (n_samples)
 
-            # Use the distances to compute interpolation coefficients
-            #  For now, we use interpolation
-            #   w_i = 1 - distance if distance < 1 else 0
-            coeff_matrix = K.clip(1 - distance_matrix, 0, 1)
+                # Shift each individual components
+                my_shifted_components = []
+                for s in range(self.n_samples):
+                    my_shift = self.shift[s, c]  # shape: ()
 
-            # Multiply the coefficient matrix by the components to
-            #  compute the interpolated components
-            #   components = NC x NCh
-            #   coeff_matrix = NS x NC x NCh X NCh
-            #  where NC is number of components, NS is the number ofsamples,
-            #  and NCh is the number of channels in the signal
-            padded_components = K.expand_dims(K.expand_dims(self.components, -1), 0)
-            self.shifted_components = K.squeeze(tf.matmul(
-                coeff_matrix,
-                padded_components
-            ), 3)
+                    # Compute the distance from each point
+                    distance_matrix = K.abs(my_shift - k_matrix)
+                    coeff_matrix = K.clip(1 - distance_matrix, 0, 1)
 
-            # Multiply the shifted coefficients by the contribution per component
-            #  The shifted component matrix is NS x NC x NCh
-            #  The contributions matrix is NS x NC
-            self.shifted_contributions = tf.matmul(
-                K.expand_dims(self.contributions, 1),
-                self.shifted_components
-            )
+                    # Compute the shift
+                    shifted_component = K.dot(coeff_matrix,
+                                              K.expand_dims(my_component, -1))
+                    my_shifted_components.append(K.squeeze(shifted_component, -1))
 
-            # Now, sum up the contributions
-            #  The shifted contribution matrix is NS x NC x NCh
-            return K.sum(self.shifted_contributions, axis=1, keepdims=False)
+                # Stack them to form: (n_samples, n_channels)
+                my_shifted_components = tf.stack(my_shifted_components,
+                                                 name=f'shifted_components_{c}')
+
+                # Multiply by the contribution ot each sample
+                my_shifted_contribution = tf.multiply(
+                    my_shifted_components,
+                    K.expand_dims(my_contribution, -1)
+                )
+                shifted_components.append(tf.stack(my_shifted_contribution))
+
+            # Stacked array has a shape: (n_components, n_samples, n_channels)
+            self.shifted_contributions = tf.stack(shifted_components, 0,
+                                                  name='shifted_contributions')
+
+            # The total signal is the sum of all components
+            return K.sum(self.shifted_contributions, axis=0, keepdims=False)
         else:
             # Matrix multiply the contributes with the components
             #  Contributions is: NS x NC
